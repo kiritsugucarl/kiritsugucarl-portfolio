78f920dc72ec44701af9765ad5d5bfd86693a181
Kiritsugucarl <bluesniper017@gmail.com>
2023-08-07 17:00:01 +0800

    update: projects and minor additions

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () =>{
         <section id='contact' className='contact container section'>
             <h2 className='contact__title'>Did I Get Your Attention?</h2>
             <p className='contact__description'>
-                Whether you are starting a project, business inquiries, or you just want to say hi, my inbox will always be open so feel free to reach out and I will get back to you as soon as possible.
+                Whether you are starting a project, have business inquiries, or you just want to say hi, my inbox will always be open so feel free to reach out and I will get back to you as soon as possible.
             </p>
             <a className='contact__btn btn red-btn' href='mailto:cdbpineda02@gmail.com' target='_blank' rel="noopener noreferrer">Reach Out</a>
         </section>
